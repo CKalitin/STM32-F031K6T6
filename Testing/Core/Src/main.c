10e69b6859136fbd580a4f8f8ed289ed2c007065
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2024-12-07 22:37:42 -0800

    Shorter delay, 2 line display

--- Testing/Core/Src/main.c
+++ Testing/Core/Src/main.c
@@ -239,30 +239,13 @@ static void LCD_Init(){
 	HAL_GPIO_WritePin(POWER_GPIO_Port, POWER_Pin, 1);
 	HAL_Delay(100);
 
-	// Set to 4-bit mode
-	// Because this command is only 4 bits (and not two pulses of 4 bits to get 8 in total), it is done manually
-	/*HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, 0);
-	HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, 0);
-	HAL_GPIO_WritePin(D5_GPIO_Port, D5_Pin, 0);
-	HAL_GPIO_WritePin(D6_GPIO_Port, D6_Pin, 0);
-	HAL_GPIO_WritePin(D7_GPIO_Port, D7_Pin, 0);
-	LCD_E_Pulse();
-
-	HAL_GPIO_WritePin(RS_GPIO_Port, RS_Pin, 0);
-	HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, 0);
-	HAL_GPIO_WritePin(D5_GPIO_Port, D5_Pin, 1);
-	HAL_GPIO_WritePin(D6_GPIO_Port, D6_Pin, 0);
-	HAL_GPIO_WritePin(D7_GPIO_Port, D7_Pin, 0);
-	LCD_E_Pulse();
-	*/
-
 	// Start up functions:
 	// 1. 4-bit mode 0x02, 2. TODO FINISH THIS
 
 	LCD_Send(0, 0b00000010); // Set to 4-bit operation
-	HAL_Delay(100);
+	HAL_Delay(20);
 
-	LCD_Send(0, 0b00100001); // Set font to 0,0
+	LCD_Send(0, 0b00101100); // Set display to 2 rows and font to 0
 	LCD_Send(0, 0b00001110); // Turn on display and cursor appears
 	LCD_Send(0, 0b00000110); // Increase address by one, shift cursor to right when writing, display has no shift
 
@@ -280,10 +263,14 @@ static void LCD_Init(){
 	LCD_Send(1, 0b01000101); // E
 	LCD_Send(1, 0b01010010); // R
 
+	LCD_Send(1, 0b00100000); // Space
+
 	LCD_Send(1, 0b01010111); // W
 	LCD_Send(1, 0b01001111); // O
 	LCD_Send(1, 0b01010010); // R
 	LCD_Send(1, 0b01001011); // K
+
+	LCD_Send(0, 0b11000000); // Switch to second line - is this a register select?
 
 	LCD_Send(1, 0b00100000); // Space
 
@@ -322,13 +309,15 @@ static void LCD_Send(uint8_t RS_Pin_value, uint8_t data){
 }
 
 // We need to pulse the "Enable" pin to read the data pins
+// Operation enable signal. Falling edge triggers reading data.
 static void LCD_E_Pulse() {
-	// Operation enable signal. Falling edge triggered.
 	// The display reads in the values from the data pins when it detects the falling edge of the E signal
 	HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, 1);
-	HAL_Delay(100); // Slight delay for the sake of the display, probably needed idk I haven't learned this from first principles
 	HAL_GPIO_WritePin(E_GPIO_Port, E_Pin, 0);
-	HAL_Delay(100);
+
+  // After testing various delays, this one was found to be necessary
+  // This makes sense because the display should only need a delay to process after the falling edge of the E signal, as this is what triggers the read
+	HAL_Delay(1);
 }
 
 /* USER CODE END 4 */
