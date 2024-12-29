452612689217dd2ecc4ab25a97d0a0adaab4acdb
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2024-12-28 21:10:30 -0800

    Added Documentation to the ADC Auto Testing SCPI Script

--- ADC-Auto-Characterization/SCPI Scripts/PyVisa-CharacterizationScript.py
+++ ADC-Auto-Characterization/SCPI Scripts/PyVisa-CharacterizationScript.py
@@ -12,7 +12,7 @@ afg = rm.open_resource('USB0::1689::851::1516313::0::INSTR')
 # Open Digital Multimeter
 dmm = rm.open_resource('USB0::0x05E6::0x2110::8005314::INSTR')
 
-# Print devices
+# Print devices - two of these IDs are the ones used above (note that libusb had to be used to set them up)
 print(afg.query('*IDN?'))
 
 ### SETUP AFG ###
@@ -26,17 +26,12 @@ afg.write('*CLS')
 
 time.sleep(1)
 
-# Query amplitude voltage
 afg.write("SOURce1:FUNCtion:SHAPe PULSe")
 
 time.sleep(1)
 
+# Set Duty Cycle of the pulse to 99.9%
 afg.write("SOURce1:PULSe:DCYC 99.9")
-
-# enable output
-time.sleep(1)
-
-afg.write('OUTPut1:STATe ON')
 
 time.sleep(1)
 
@@ -45,10 +40,14 @@ afg.write("SOURce1:VOLTage:LEVel:IMMediate:OFFSet 0.5")
     
 time.sleep(1)
 
-dmm.query('MEAS:VOLT:DC?')
+# enable output
+# Note that the sleep before this is required, otherwise the AFG doesn't have time to set parameters
+afg.write('OUTPut1:STATe ON')
+
+time.sleep(1)
 
 seconds_per_observation = 1 # seconds
-voltage = -0.02
+voltage = -0.02 # Set to -voltageStep initially to get 0 as your first value
 
 previous_dmm_voltage = 0
 
@@ -63,14 +62,20 @@ while True:
     print(f"Voltage: {voltage}, Offset: {offset}")
     
     start_time = time.time_ns() # get current time in nanoseconds
+
+    # Set amplitude to the desired voltage, then because this is a function centered on y=0, offset it by half the desired voltage
     afg.write("SOURce1:VOLTage:LEVel:IMMediate:AMPLitude " + str(voltage))
     afg.write("SOURce1:VOLTage:LEVel:IMMediate:OFFSet " + str(offset))
-    time.sleep(0.01)
+    
+    time.sleep(0.01) # Wait after setting the AFG parameters to taking the DMM reading
     previous_dmm_voltage = float(dmm.query('MEAS:VOLT:DC?'))
+    
     print(previous_dmm_voltage)
-    f.write(f"{time.time_ns()/1e9}, {voltage}, {previous_dmm_voltage}\n") # Time (s), AFG Voltage (V), DMM Voltage (V)
+    
+    f.write(f"{time.time_ns()/1e9}, {voltage}, {previous_dmm_voltage}\n") # Write Time (s), AFG Voltage (V), DMM Voltage (V)
+    
     end_time = time.time_ns()
     
-    time.sleep(seconds_per_observation - (end_time - start_time)/1e9)
+    time.sleep(seconds_per_observation - (end_time - start_time)/1e9) # Sleep so that the next observation begins exactly 1 second after the first
 
 f.close()
