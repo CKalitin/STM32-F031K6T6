b607bc609aa127d2dc391ead5d950be7a377f797
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2025-02-10 11:53:53 -0800

    DMA sum in halves
    Now instead of the code trying to average the buffer while the DMA peripheral is trying to write into it, we use call back to average half of the buffer at a time. The half that DMA isn't writing into. So, no conflicts.

--- ADC-Auto-Characterization-Project/ECU-DMA/Core/Inc/ADC.h
+++ ADC-Auto-Characterization-Project/ECU-DMA/Core/Inc/ADC.h
@@ -1,8 +1,10 @@
 #include "main.h"
 
-#define ADC_BUFFER_LENGTH 128
+#define ADC_BUFFER_LENGTH 512
 
 uint32_t adcBuffer[ADC_BUFFER_LENGTH];
+
+uint32_t adcAveragedValue;
 
 // Note that UART_HandleTypeDef and ADC_HandleTypeDef are parameters so that we can change if we use UART1, UART2, etc. or ADC1, ADCx, etc.
 void Get_Averaged_ADC_Values(int*, int*, int*);
--- ADC-Auto-Characterization-Project/ECU-DMA/Core/Src/ADC.c
+++ ADC-Auto-Characterization-Project/ECU-DMA/Core/Src/ADC.c
@@ -7,11 +7,7 @@
   * @retval None
   */
 void Get_Averaged_ADC_Values(int* adcValuesAveraged, int* adcValuesADCAdjusted, int* adcValuesCurrentAdjusted){
-  int adcValuesSum = 0;
-
-  for (int i = 0; i < ADC_BUFFER_LENGTH; i++){
-    adcValuesSum += adcBuffer[i];
-  }
+  *adcValuesAveraged = adcAveragedValue; // Set the pointer output
 
   // ADC Error Polynomial: -75.8 + 0.0222x
   // Current Sensor ADC Error Polynomial: 72 - 0.0325x + 19.6 - 8.82E-3x = 91.6 - 0.04132x
@@ -21,4 +17,24 @@ void Get_Averaged_ADC_Values(int* adcValuesAveraged, int* adcValuesADCAdjusted, 
 
   *adcValuesADCAdjusted = *adcValuesAveraged - adcError; // Set the other pointer output
   *adcValuesCurrentAdjusted = *adcValuesAveraged - adcError - currentSensorADCError; // Set the other pointer output
+}
+
+// adc dma half complete callback
+// We can't sum in the Get_Averaged_ADC_Values function because DMA would to writing into some part of the buffer at the same time
+// REMEMBER!!!! This function might not be allowed to be outside of main.c, so if you run into issues during testing, that's probably it
+void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef* hadc){
+  int adcValuesSum = 0;
+  for (int i = 0; i < ADC_BUFFER_LENGTH / 2; i++){
+    adcValuesSum += adcBuffer[i];
+  }
+  adcAveragedValue = adcValuesSum / (ADC_BUFFER_LENGTH / 2);
+}
+
+// adc dma full complete callback
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc){
+  int adcValuesSum = 0;
+  for (int i = ADC_BUFFER_LENGTH/2; i < ADC_BUFFER_LENGTH; i++){
+    adcValuesSum += adcBuffer[i];
+  }
+  adcAveragedValue = adcValuesSum / (ADC_BUFFER_LENGTH / 2);
 }
