6d57842c76b3b9d4ed18b813afd73fa9d2493f07
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2025-02-15 19:57:54 -0800

    ADC error term constants changed

--- ADC-Auto-Characterization-Project/ECU-DMA/Core/Src/ADC.c
+++ ADC-Auto-Characterization-Project/ECU-DMA/Core/Src/ADC.c
@@ -15,8 +15,8 @@ void Get_Averaged_ADC_Values(int* adcValuesAveraged, int* adcValuesADCAdjusted, 
   // ADC Error Polynomial: -75.8 + 0.0222x
   // Current Sensor ADC Error Polynomial: 72 - 0.0325x + 19.6 - 8.82E-3x = 91.6 - 0.04132x
 
-  int adcError = -75.8 + 0.0222 * (*adcValuesAveraged); // This is a predetermined error polynomial
-  int currentSensorADCError = 91.6 - 0.04132 * (*adcValuesAveraged); // This is the predetermined erroy polynomial for the current sensor
+  int adcError = -61.1 + (0.0116 * (*adcValuesAveraged)); // This is a predetermined error polynomial
+  int currentSensorADCError = 51.1 + (-0.0239 * (*adcValuesAveraged)); // This is the predetermined erroy polynomial for the current sensor
 
   *adcValuesADCAdjusted = *adcValuesAveraged - adcError; // Set the other pointer output
   *adcValuesCurrentAdjusted = *adcValuesAveraged - adcError - currentSensorADCError; // Set the other pointer output
